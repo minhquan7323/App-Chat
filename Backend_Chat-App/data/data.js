cdb7db0e327c03725fc45e63dc86df009bc0ce52
minhquan7323 <minhquany9@gmail.com>
2024-11-06 19:31:51 +0700

    chat app

---
+++ Backend_Chat-App/data/data.js
@@ -0,0 +1,35 @@
+// data/data.js
+const chats = [
+    {
+        isGroupChat: false,
+        users: [
+            {
+                name: 'a1',
+                email: 'a1@gmail.com'
+            },
+            {
+                name: 'a2',
+                email: 'a2@gmail.com'
+            },
+        ],
+        _id: '617a077e18c25468bc7c4dd4',
+        chatName: 'a1'
+    },
+    {
+        isGroupChat: false,
+        users: [
+            {
+                name: 'test',
+                email: 'test@gmail.com'
+            },
+            {
+                name: 'a3',
+                email: 'a3@gmail.com'
+            },
+        ],
+        _id: '617a077e18c25468bc7c4dd5',
+        chatName: 'test'
+    }
+];
+
+module.exports = chats
---
+++ Backend_Chat-App/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const chats = require('./data/data')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('API is running')
+})
+
+app.get('/api/chat', (req, res) => {
+    res.send(chats)
+})
+app.get('/api/chat/:id', (req, res) => {
+    console.log('req', req.params.id)
+    const singleChat = chats.find((chat) => chat._id === req.params.id)
+    res.send(singleChat)
+})
+
+app.listen(
+    5000,
+    console.log('Server started on port 5000')
+)
