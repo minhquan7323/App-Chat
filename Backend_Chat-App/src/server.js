95ffe615852f17ab57939fd223193e5813b75ecf
minhquan7323 <minhquany9@gmail.com>
2024-11-12 07:29:03 +0700

    ok

--- Backend_Chat-App/src/server.js
+++ Backend_Chat-App/src/server.js
@@ -19,19 +19,19 @@ connectDB()
 routes(app)
 // --------------------------deployment------------------------------
 
-const __dirname1 = path.resolve()
+// const __dirname1 = path.resolve()
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname1, "../Frontend_Chat-App/build")))
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname1, "../Frontend_Chat-App/build")))
 
-    app.get("*", (req, res) =>
-        res.sendFile(path.resolve(__dirname1, "../Frontend_Chat-App/build", "index.html"))
-    )
-} else {
-    app.get("/", (req, res) => {
-        res.send("API is running..")
-    })
-}
+//     app.get("*", (req, res) =>
+//         res.sendFile(path.resolve(__dirname1, "../Frontend_Chat-App/build", "index.html"))
+//     )
+// } else {
+app.get("/", (req, res) => {
+    res.send("API is running..")
+})
+// }
 
 // --------------------------deployment------------------------------
 app.use(notFound)
