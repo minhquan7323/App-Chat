af29e9766eaae13d1e2492c3a9774c24e7534275
minhquan7323 <minhquany9@gmail.com>
2024-12-27 23:46:26 +0700

    ok

--- Backend_Chat-App/src/server.js
+++ Backend_Chat-App/src/server.js
@@ -19,26 +19,26 @@ connectDB()
 routes(app)
 // --------------------------deployment------------------------------
 
-// const __dirname1 = path.resolve()
+const __dirname1 = path.resolve()
 
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static(path.join(__dirname1, "../Frontend_Chat-App/build")))
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname1, "../Frontend_Chat-App/build")))
 
-//     app.get("*", (req, res) =>
-//         res.sendFile(path.resolve(__dirname1, "../Frontend_Chat-App/build", "index.html"))
-//     )
-// } else {
-app.get("/", (req, res) => {
-    res.send("API is running..")
-})
-// }
+    app.get("*", (req, res) =>
+        res.sendFile(path.resolve(__dirname1, "../Frontend_Chat-App/build", "index.html"))
+    )
+} else {
+    app.get("/", (req, res) => {
+        res.send("API is running..")
+    })
+}
 
 // --------------------------deployment------------------------------
 app.use(notFound)
 app.use(ErrorHandler)
 
 const server = app.listen(port, () => {
-    // console.log('Server is running on port', port)
+    console.log('Server is running on port', port)
 })
 const io = require('socket.io')(server, {
     pingTimeOut: 60000,
