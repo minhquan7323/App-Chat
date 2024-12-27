b63ad36eedda350a11a5824b4104aa54a594d224
minhquan7323 <minhquany9@gmail.com>
2024-12-28 00:35:07 +0700

    ok

---
+++ Frontend_Chat-App/src/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader', // Hoặc bỏ qua nếu không dùng Babel
+                },
+            },
+        ],
+    },
+};
